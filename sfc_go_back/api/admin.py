667d1d44ae2e4da895e2d755ed30c77fb2735f29
Qiao Hakusai <zbc0319@live.jp>
2022-09-19 03:35:23 +0900

    Update admin.py

--- sfc_go_back/api/admin.py
+++ sfc_go_back/api/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import M5sensor, Checkpoint
+from .models import M5sensor, Checkpoint, M5sensorHeartrate
 # Register your models here.
 
 class M5dataAdmin(admin.ModelAdmin):
-    list_display = ['m5tagid', 'temperature', 'humidity', 'x_axis', 'y_axis', 'z_axis', 'timestamp']
+    list_display = ['m5tagid', 'temperature', 'humidity', 'x_axis', 'y_axis', 'z_axis', 'timestamp','ble_poi']
 
 class CheckpointAdmin(admin.ModelAdmin):
     list_display = ['checkpointtitle', 'latitude', 'longitude', 'timestamp']
 
+class M5sensorHeartrateAdmin(admin.ModelAdmin):
+    list_display = ['m5tagid', 'heartrate', 'timestamp','ble_poi']
+
 admin.site.register(M5sensor,M5dataAdmin)
 admin.site.register(Checkpoint,CheckpointAdmin)
+admin.site.register(M5sensorHeartrate,M5sensorHeartrateAdmin)
